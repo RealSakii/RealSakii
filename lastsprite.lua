9f70174123c46f45509a890c9435ef4a76ed868b
RealSakii <maruchop7829@gmail.com>
2026-01-15 11:05:50 -0300

    Update and rename Jailbreak.lua to lastsprite.lua

---
+++ lastsprite.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RealSakii/RealSakii/refs/heads/main/Aimbot.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RealSakii/RealSakii/refs/heads/main/m1%20reset%20dash))()
