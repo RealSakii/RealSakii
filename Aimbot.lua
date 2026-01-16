7110fc3455085f8cfbfddb854050127d9e0d6d5e
RealSakii <maruchop7829@gmail.com>
2026-01-16 20:55:09 +0700

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -10,7 +10,7 @@ local Mouse = LocalPlayer:GetMouse()
 -- SETTINGS
 local AIM_KEY = Enum.UserInputType.MouseButton2
 local FOV_RADIUS = 500
-local MAX_DISTANCE = 10
+local MAX_DISTANCE = 30
 local SMOOTHNESS = 1.5
 local AIM_PART = "HumanoidRootPart"
 
@@ -95,5 +95,6 @@ if not getgenv().DisableNotification then
 		Callback = function() end
 	})
 end
+
 
 
